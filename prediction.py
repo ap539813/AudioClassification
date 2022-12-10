9f7c35f088525a6177bf8b63dc7b6a4c95a8f996
Ankush Pandey <ap539813@gmail.com>
2022-12-10 21:32:01 +0530

    Update prediction.py

--- prediction.py
+++ prediction.py
@@ -1,12 +1,7 @@
 
-from create_models import load_model, get_results
-from process_data import scale_data, get_label
-from assets import column_names, mode_data, months, neighbourhood, object_cols
-from assets import division, location_type, premises_type, ucr_code, ucr_ext, offence
+from create_models import load_model
+from process_data import scale_data
 import streamlit as st
-import pandas as pd
-import pickle
-import datetime
 
 from audiorecorder import audiorecorder
 
@@ -14,35 +9,32 @@ from audiorecorder import audiorecorder
 def make_prediction(model_path):
     model = load_model(model_path)
     c11, c12 = st.columns([1, 1])
-    c11.markdown('### Show model performance: ')
-    if c12.checkbox(''):
-        get_results(model)
-    else:
+    # c11.markdown('### Show model performance: ')
+    # if c12.checkbox(''):
+    #     get_results(model)
+    if True:
         st.title("Audio Recorder")
         audio = audiorecorder("Click to record", "Recording click to stop...")
+        if len(audio) <= 64082:
+            pass
         if len(audio) > 0:
             # To play audio in frontend:
             st.audio(audio)
             
             # To save audio to a file:
-            wav_file = open("audio.wav", "wb")
+            wav_file = open("sample_collected/audio.wav", "wb")
             wav_file.write(audio.tobytes())
+            wav_file.close()
 
-            st.button('Authenticate')
-        # print(result)
-        # input_X = 
-        # X = pd.DataFrame(input_X)
-        # encoders = pickle.load(open('saved_models/LabelEncoder', 'rb'))
-        # for col in object_cols:
-        #     X[col] = encoders[col].transform(X[col])
-        
-        # predict = st.button('Make Prediction >>>')
-        # if predict:
-        #     X = scale_data(X)
-        #     pred = get_label(model.predict(X)[0])
+            st.write(audio.max())
 
+            predict = st.button('Authenticate')
+            if predict:
+                X = scale_data(audio.reshape(1, -1))
+                st.write(X.max())
+                pred = model.predict(X)[0]
 
-        #     st.success(f'The Predicted label is: {pred}')
+                st.success(f'The age of the speeker is close to: {pred}')
 
         
         
